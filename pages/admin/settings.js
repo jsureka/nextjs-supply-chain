778b6c2dc8b5369c780aa6a2e021fb504c907321
jsureka <bsse1115@iit.du.ac.bd>
2022-09-22 02:03:25 +0600

    Worker and product form created

--- pages/admin/settings.js
+++ pages/admin/settings.js
@@ -2,22 +2,24 @@ import React from "react";
 
 // components
 
-import CardSettings from "components/Cards/CardSettings.js";
+import CardSettings from "components/Cards/CardWorker.js";
 import CardProfile from "components/Cards/CardProfile.js";
 
 // layout for page
 
 import Admin from "layouts/Admin.js";
+import CardWorker from "components/Cards/CardWorker.js";
+import CardProduct from "components/Cards/CardProduct";
 
 export default function Settings() {
   return (
     <>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-8/12 px-4">
-          <CardSettings />
+          <CardWorker />
         </div>
         <div className="w-full lg:w-4/12 px-4">
-          <CardProfile />
+          <CardProduct />
         </div>
       </div>
     </>
